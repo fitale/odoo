7e84f0bd8c67311db570c1da9ee00d2be05ea8f3
fitale <fitalewari@gmail.com>
2021-07-22 12:57:51 -0400

    set up divs and added one onClick event to show what I'd do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let count = 0;
+
+  let renderBoxes = () => {
+    return (
+      <>
+        <section className="section">
+          <div onClick={(box) => console.log(box)} className="rows 1" id="box1">
+            {count}
+          </div>
+          <div className="rows 2">{count}</div>
+          <div className="rows 3">{count}</div>
+        </section>
+        <section className="section">
+          <div className="rows 1">{count}</div>
+          <div className="rows 2">{count}</div>
+          <div className="rows 3">{count}</div>
+        </section>
+        <section className="section">
+          <div className="rows 1">{count}</div>
+          <div className="rows 2">{count}</div>
+          <div className="rows 3">{count}</div>
+        </section>
+      </>
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{renderBoxes()}</header>
     </div>
   );
 }
